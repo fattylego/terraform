29ca1bdaf8c4466ee46a7161a6a8ee2fb058b9cd
fattylego <mhkhong@gmail.com>
2021-04-24 21:39:16 +0800

    Create main.tf
    my first terraform

---
+++ main.tf
@@ -0,0 +1,24 @@
+#Define keys and region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+        #shared_credentials_file = "~/.aws/credentials"
+        region = "us-west-2"
+}
+
+#Define s3 bucket
+resource "aws_s3_bucket" "bucket1" {
+        bucket = "bucket-launched-using-terrafrom-20210424"
+        acl = "private" # or can be "public-read"
+        tags = {
+                Name = "Bucket"
+                Environment = "Production"
+        }
+}
