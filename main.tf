2ff5cb5c8de7e2eb2ed8be706007bed3f167c28b
fattylego <mhkhong@gmail.com>
2021-04-25 11:13:44 +0800

    Update main.tf
    include backend

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "s3" {
+    bucket  = "terraform-remote-store-25042021"
+    encrypt = true
+    key     = "terraform-jenkins.tfstate"
+    region  = "us-west-2"
+  }
 }
 
 provider "aws" {
@@ -14,10 +21,10 @@ provider "aws" {
 
 #Define s3 bucket
 resource "aws_s3_bucket" "bucket1" {
-        bucket = "bucket-launched-using-terrafrom-20210424"
+        bucket = "bucket-launched-using-terrafrom-20210423"
         acl = "private" # or can be "public-read"
         tags = {
                 Name = "Bucket"
-                Environment = "Production"
+                Environment = "Jenkins"
         }
 }
